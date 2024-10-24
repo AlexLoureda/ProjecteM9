63cdd1809b1b1ce4f272c3a38aa4456b07bcaa9d
Alex Loureda <alex.loureda@cirvianum.cat>
2024-10-24 18:33:23 +0200

    funciona, 50% fet

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,13 +5,19 @@ module.exports = {
     "./src/**/*.{html,js}", // Scan HTML and JS files in src
   ],
   theme: {
-    extend: {},
-    colors: {
-      primary: "#f5f5f5", // bg-gray-100
-      secondary1: "#86EFAC", // bg-green-300
-      secondary2: "#93C5FD", // bg-blue-300
-      tertiary: "#1A3D40",
+    extend: {
+      colors: {
+        primary: "#f5f5f5",
+        secondary1: "#ecfcce",
+        secondary2: "#bbd5f5",
+        tertiary: "#1A3D40",
+        blanc: "#fafafa",
+        negre: "#000000",
+        gray: "#4B5563",
+        blue500: "#3B82F6",
+      },
     },
+    
   },
   plugins: [],
 };
